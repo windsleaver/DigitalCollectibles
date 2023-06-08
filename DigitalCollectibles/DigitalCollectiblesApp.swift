cdf4889e7d78f0dc5473db8247ae94c8446fabe8
windsleaver <windsleaver@gmail.com>
2023-06-09 00:11:09 +0800

    Initial Commit

---
+++ DigitalCollectibles/DigitalCollectiblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalCollectiblesApp.swift
+//  DigitalCollectibles
+//
+//  Created by Winds Leaver on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct DigitalCollectiblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
